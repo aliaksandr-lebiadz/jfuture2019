fd38e479ed2038bbff158d668fd5589f90371c61
Aliaksandr Lebiadz <sasha_lebed2001@mail.ru>
2019-11-11 22:16:19 +0300

    Refactor some methods and rename

--- src/main/java/dev/jfuture/task/parser/director/impl/DirectorsParserImpl.java
+++ src/main/java/dev/jfuture/task/parser/director/impl/DirectorsParserImpl.java
@@ -16,49 +16,39 @@ import java.util.Optional;
 public class DirectorsParserImpl implements DirectorsParser {
 
     private static final String SOURCE = "https://www.imdb.com/chart/top";
-    private static final String TABLE_CLASS = "chart full-width";
+    private static final String TABLE_CLASS = "chart";
     private static final String TITLE_CLASS = "titleColumn";
-    private static final String RATING_CLASS = "ratingColumn imdbRating";
+    private static final String RATING_CLASS = "ratingColumn";
     private static final String ROW_TAG = "tr";
     private static final String HYPERLINK_TAG = "a";
 
     private static final String DIRECTOR_ATTRIBUTE = "title";
-    private static final String DIRECTOR_LABEL = "(dir.)";
+    private static final String DIRECTOR = "(dir.)";
 
     private static final int HEADER_INDEX = 0;
 
     @Override
     public List<Movie> getTopMoviesByLimit(int amount) throws WebsiteParsingException {
-        Document document;
-        try {
-            document = Jsoup.connect(SOURCE).get();
-        } catch (IOException ex) {
-            throw new WebsiteParsingException(ex.getMessage(), ex);
-        }
-        Element body = document.body();
-        Element table = body.getElementsByClass(TABLE_CLASS).first();
-        Elements rows = table.select(ROW_TAG);
-        removeHeader(rows);
-
-        List<Movie> movies = new ArrayList<>(amount);
-        for (int i = 0; movies.size() < 5; i++) {
+        Elements rows = getTableRows();
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; movies.size() < amount; i++) {
             Element row = rows.get(i);
-            Element titleAndDirectorAndCast = row.getElementsByClass(TITLE_CLASS)
+            Element titleAndDirectorElement = row.getElementsByClass(TITLE_CLASS)
                     .first()
                     .selectFirst(HYPERLINK_TAG);
-            String title = titleAndDirectorAndCast.text();
-            String director = parseDirector(titleAndDirectorAndCast);
+            String title = titleAndDirectorElement.text();
+            String director = getDirector(titleAndDirectorElement);
             String ratingValue = row.getElementsByClass(RATING_CLASS)
                     .first()
                     .text();
             double rating = Double.parseDouble(ratingValue);
             Movie movie = new Movie(title, director, rating);
 
-            Optional<Movie> movieWithSameDirectorOptional = getMovieWithDirector(movies, director);
+            Optional<Movie> movieWithSameDirectorOptional = getMovieWithSuppliedDirector(movies, director);
             if (movieWithSameDirectorOptional.isPresent()) {
-                Movie oldMove = movieWithSameDirectorOptional.get();
-                if (shouldReplace(oldMove, movie)) {
-                    int index = movies.indexOf(oldMove);
+                Movie oldMovie = movieWithSameDirectorOptional.get();
+                if (needsReplace(oldMovie, movie)) {
+                    int index = movies.indexOf(oldMovie);
                     movies.set(index, movie);
                 }
             } else {
@@ -69,23 +59,33 @@ public class DirectorsParserImpl implements DirectorsParser {
         return movies;
     }
 
-    private void removeHeader(Elements rows) {
-        rows.remove(HEADER_INDEX);
-    }
-
-    private String parseDirector(Element titleAndDirector) {
+    private String getDirector(Element titleAndDirector) {
         String directorAndCast = titleAndDirector.attr(DIRECTOR_ATTRIBUTE);
-        int directorLabelIndex = directorAndCast.indexOf(DIRECTOR_LABEL);
+        int directorLabelIndex = directorAndCast.indexOf(DIRECTOR);
         return directorAndCast.substring(0, directorLabelIndex).trim();
     }
 
-    private Optional<Movie> getMovieWithDirector(List<Movie> movies, String director) {
+    private Optional<Movie> getMovieWithSuppliedDirector(List<Movie> movies, String director) {
         return movies.stream()
                 .filter(m -> m.getDirector().equals(director))
                 .findFirst();
     }
 
-    private boolean shouldReplace(Movie oldMovie, Movie movie) {
+    private boolean needsReplace(Movie oldMovie, Movie movie) {
         return movie.getRating() > oldMovie.getRating();
+    }
+
+    private Elements getTableRows() throws WebsiteParsingException{
+        Document document;
+        try {
+            document = Jsoup.connect(SOURCE).get();
+            Element body = document.body();
+            Element table = body.getElementsByClass(TABLE_CLASS).first();
+            Elements rows = table.select(ROW_TAG);
+            rows.remove(HEADER_INDEX); // removing header
+            return rows;
+        } catch (IOException ex) {
+            throw new WebsiteParsingException(ex.getMessage(), ex);
+        }
     }
 }
